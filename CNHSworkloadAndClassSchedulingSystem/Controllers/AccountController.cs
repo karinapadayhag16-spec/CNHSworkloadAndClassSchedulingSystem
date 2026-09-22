12cdfc3455fd95b4b7a863a776a5d7bd7cd905ef
karinapadayhag16-spec <karinapadayhag16@gmail.com>
2026-09-22 15:52:32 +0800

    Add AccountController and LogIn view
    Added AccountController with Index and LogIn actions returning views. Created LogIn.cshtml Razor view with placeholders. Updated csproj file (possible BOM added, no functional changes).

---
+++ CNHSworkloadAndClassSchedulingSystem/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CNHSworkloadAndClassSchedulingSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+    }
+}
