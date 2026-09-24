346a29e1c0a96221d2fd836170095aed3f52e76b
Skoshiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <ninoapale0@gmail.com>
2026-09-24 11:20:02 +0800

    Set Account/Login as default route; UI and logo updates
    - Changed default route to Account/Login in Program.cs
    - Added mt-5 margin to login card in LogIn.cshtml
    - Updated school logo image source to logo.png

--- CNHSworkloadAndClassSchedulingSystem/Program.cs
+++ CNHSworkloadAndClassSchedulingSystem/Program.cs
@@ -27,7 +27,7 @@ namespace CNHSworkloadAndClassSchedulingSystem
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Account}/{action=Login}/{id?}")
                 .WithStaticAssets();
 
             app.Run();
